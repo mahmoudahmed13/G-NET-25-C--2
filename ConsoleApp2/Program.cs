150a819557e86b4d63ace3022ff8bfc9674be28e
mahmoudahmed13 <mahmoudahmed13@gmail.com>
2026-02-11 02:10:31 +0200

    Question01

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            //Q1: What will this print and explain what happens ?
+
+            //double d = 9.99;
+            //int x = (int)d; //Overlodaing
+            //Console.WriteLine(x); // 9 >> because this is explicit casting cause data loss
+            #endregion
         }
     }
 }
